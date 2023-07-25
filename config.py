83bc89138a3a9326c37d2f3bc936e96e548e2935
evilplayer666 <89117699+evilplayer666@users.noreply.github.com>
2023-07-25 16:19:03 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "unx.chozvldepquz.ap-south-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "123456789"
+customdb = "employee"
+custombucket = "chinmadarchod"
+customregion = "ap-south-1"
